2f1360b5486269deee2b8947d27b11e05b677bc3
su <158677070@qq.com>
2024-02-26 16:01:45 +0800

    添加 UserState 用户状态表 以及 UserState 对应实体类、Mapper、Service

---
+++ service/user/src/main/java/com/syt/user/mapper/data/UserStateMapper.java
@@ -0,0 +1,10 @@
+package com.syt.user.mapper.data;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.syt.model.user.dos.UserState;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserStateMapper extends BaseMapper<UserState> {
+
+}
---
+++ service/user/src/main/java/com/syt/user/service/data/UserStateService.java
@@ -0,0 +1,8 @@
+package com.syt.user.service.data;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.syt.model.user.dos.UserState;
+
+public interface UserStateService extends IService<UserState> {
+
+}
