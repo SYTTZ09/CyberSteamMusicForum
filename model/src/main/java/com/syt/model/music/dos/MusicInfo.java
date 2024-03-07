51a99560242f97c3a5e6490dcd3d5d4fb5eebd69
su <158677070@qq.com>
2024-03-08 00:21:19 +0800

    为 createTime 字段添加 json 格式化 将 Date 类型格式化为 "yyyy年MM月dd日"

--- model/src/main/java/com/syt/model/music/dos/MusicInfo.java
+++ model/src/main/java/com/syt/model/music/dos/MusicInfo.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -54,6 +55,7 @@ public class MusicInfo implements Serializable {
     /**
      * 创建时间
      */
+    @JsonFormat(pattern = "yyyy年MM月dd日")
     @TableField("create_time")
     private Date createTime;
 
