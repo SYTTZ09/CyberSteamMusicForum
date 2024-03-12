c749a0a6f49a057f5af9085f0975066ffa7bc6d3
su <158677070@qq.com>
2024-03-12 17:07:35 +0800

    添加音乐收藏夹对应mapper

---
+++ service/music/src/main/java/com/syt/music/mapper/data/MusicCollectionMapper.java
@@ -0,0 +1,10 @@
+package com.syt.music.mapper.data;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.syt.model.music.dos.MusicCollection;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MusicCollectionMapper extends BaseMapper<MusicCollection> {
+
+}
